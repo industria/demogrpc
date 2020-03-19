a239d53ac33aa5ff62118ad2e65941c56344bf4e
James Lindstorff <james.lindstorff@jppol.dk>
2020-03-19 11:22:31 +0100

    s

---
+++ src/main/java/dk/eb/sample/grpc/demo/DemoClient.java
@@ -0,0 +1,39 @@
+package dk.eb.sample.grpc.demo;
+
+import dk.eb.sample.grpc.demo.DemoThingsGrpc.DemoThingsBlockingStub;
+import io.grpc.Channel;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ */
+public class DemoClient {
+
+    private final DemoThingsBlockingStub blockingStub;
+
+    public DemoClient(Channel channel) {
+        this.blockingStub = DemoThingsGrpc.newBlockingStub(channel);
+    }
+
+    public Thing thing(int id) {
+        Some request = Some.newBuilder().setId(id).build();
+        return blockingStub.getSomeThing(request);
+    }
+    
+    public static void main(String[] args) throws Exception {
+
+        ManagedChannel channel = ManagedChannelBuilder.forTarget("localhost:8777").usePlaintext().build();
+        try {
+            DemoClient client = new DemoClient(channel);
+
+            Thing thing = client.thing(0);
+            
+            System.out.println("Thing: " + thing.getId() + " with value: " + thing.getValue());
+        } finally {
+            channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
+        }
+    }
+
+}
---
+++ src/main/java/dk/eb/sample/grpc/demo/DemoServer.java
@@ -0,0 +1,84 @@
+package dk.eb.sample.grpc.demo;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ */
+public class DemoServer {
+
+    private final Server server;
+
+    public DemoServer() {
+        this.server = ServerBuilder.forPort(8777).addService(new DemoThingService()).build();
+    }
+
+    /**
+     * Start serving requests.
+     */
+    public void start() throws IOException {
+        server.start();
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+                System.err.println("*** shutting down gRPC server since JVM is shutting down");
+                try {
+                    DemoServer.this.stop();
+                } catch (InterruptedException e) {
+                    e.printStackTrace(System.err);
+                }
+                System.err.println("*** server shut down");
+            }
+        });
+    }
+
+    /**
+     * Stop serving requests and shutdown resources.
+     */
+    public void stop() throws InterruptedException {
+        if (server != null) {
+            server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+        }
+    }
+
+    /**
+     * Await termination on the main thread since the grpc library uses daemon
+     * threads.
+     */
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        DemoServer server = new DemoServer();
+        server.start();
+        server.blockUntilShutdown();
+    }
+
+    private static class DemoThingService extends DemoThingsGrpc.DemoThingsImplBase {
+
+        DemoThingService() {
+
+        }
+
+        private Thing findThing(int id) {
+            return Thing.newBuilder().setId(id).setValue("Found").build();
+        }
+
+        @Override
+        public void getSomeThing(Some request, StreamObserver<Thing> responseObserver) {
+
+            Thing thing = findThing(request.getId());
+            responseObserver.onNext(thing);
+            responseObserver.onCompleted();
+        }
+
+    }
+}
